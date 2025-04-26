bf7482bcfdc0f256c13ef7e47107cdcce42c3a38
Kaushal5033 <kaushalrai8076958922@gmail.com>
2025-04-26 09:28:08 +0530

    Port Binding Fix

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,15 +1,14 @@
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-export const initSocket = async () =>{
+export const initSocket = async () => {
     const options = {
-        'force new connection': true,
         reconnectionAttempts: 5,
         timeout: 20000,
-        transports: ['websocket', 'polling'],
+        transports: ['websocket', 'polling'], // Use WebSocket and Polling
         autoConnect: true,
         reconnection: true,
         reconnectionDelay: 1000,
         reconnectionDelayMax: 5000,
     };
     return io(process.env.REACT_APP_BACKEND_URL, options);
-}
+};
